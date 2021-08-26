449aae1477f3b975160513d21ac90b92c88d129e
Ray Agni <jagni001@ucr.edu>
2021-08-25 17:59:44 -0700

    Added cards and other things'

--- friender-frontend/src/matches/Matches.js
+++ friender-frontend/src/matches/Matches.js
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../UserContext";
+import Match from "./Match";
 
 /** Matches
  * Show the current user's matches
@@ -14,10 +15,24 @@ function Matches({ getMatches }) {
   const currentUser = useContext(UserContext);
   const history = useHistory();
   if (!currentUser) history.push("/");
+
+  const [matches, setMatches] = useState([1, 2, 3, 4, 5, 6]);
+
   /** TODO: fetch list of user's matches from the API
   /* TODO: add */
 
-  return <div className="Matches"> DEV IN PROGRESS </div>;
+  return (
+    <div className="Matches container">
+      <h1>
+        <i>
+          <u> Your Matches</u>
+          {matches.map((m) => (
+            <Match match={m} />
+          ))}
+        </i>
+      </h1>
+    </div>
+  );
 }
 
 export default Matches;
