02304f1699f378ff8f7ef458721af51d5acfdead
Wukongthethird <simonzhang9518@gmail.com>
2021-08-26 15:50:48 -0700

    form validator

--- friender-backend/forms.py
+++ friender-backend/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, NumberRange
 
 class UserRegisterForm(FlaskForm):
     """Form for registering users."""
@@ -11,7 +11,7 @@ class UserRegisterForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
     zipcode = StringField('Zipcode', validators=[DataRequired()])
-    friend_radius = IntegerField('Friend Radius',  validators=[DataRequired()] )
+    friend_radius = IntegerField('Friend Radius',  validators=[DataRequired(), NumberRange(min=10, max=100) ] )
 
 class LoginForm(FlaskForm):
     """Login Form"""
