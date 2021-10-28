9c9943c052ac14c2f259c56dfa9f7234f2991e08
Ray Agni <jagni001@ucr.edu>
2021-10-27 18:34:46 -0700

    FEAT: added routes
    DESCRIPTION: added routes to the friender backend

--- friender-backend/app.py
+++ friender-backend/app.py
@@ -1,7 +1,6 @@
-
-from flask import Flask, render_template, redirect, g
+"""Models for Friender."""
+from flask import Flask, g, request, jsonify
 from models import db, connect_db, User
-from forms import UserForm
 from flask_debugtoolbar import DebugToolbarExtension
 
 
@@ -15,14 +14,45 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
-@app.route('/auth/login', methods=['POST']
+@app.route('/api/auth/login', methods=['POST'])
 def login():
-    """ Authenticates user from login form credentials
+    """Authenticate user from login form credentials.
 
-    Returns JSON {'user': {id, first_name, last_name}}
+    Returns JSON {'user': {id, first_name, last_name, token}}
     """
-    serialize
-    # query user info from DB
+
+    #TODO: build out login
     pass
 
+@app.route('/api/auth/register', methods=['POST'])
+def register():
+    """Register user from form data.
 
+    Returns JSON {'users': {id, first_name, last_name, token}} 
+    """
+
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    password = request.json['password']
+    img_url = request.json['img_url']
+
+    new_user = User.register(first_name=first_name, 
+                             last_name=last_name,
+                             email=email, 
+                             password=password, 
+                             img_url=img_url) 
+
+    db.session.commit()
+    g.user = new_user
+    serialized = new_user.serialize()
+
+    # Return w/status code 201 --- return tuple (json, status)
+    return (jsonify(dessert=serialized), 201)
+
+
+# TODO: Create a route for GET current user's matches
+# TODO: Create a route for POST matching user with selected user
+# TODO: Create a route Getting all messages between two users
+# TODO: create a route for Posting a Message from one user to another user
+# TODO: Create a route for editing the current user
--- friender-backend/models.py
+++ friender-backend/models.py
@@ -1,12 +1,14 @@
-""" Models for Friender"""
+"""Models for Friender."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+import bcrypt
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
 
 def connect_db(app):
-    """ initializes and connects the models to the app"""
+    """Connect SQLAlchemy to application."""
     db.app = app
     db.init_app(app)
 
@@ -28,16 +30,31 @@ class User(db.Model):
 
     # TODO: Add user image to database via correct path, for now will use url
     img_url = db.Column(db.String(50), nullable=False)
-    
-    matches = db.relationship("Matches", 
-                               backref="user", 
-                               secondary="Matches")
+
+    matches = db.relationship("Match")
 
     messages = db.relationship("Message")
 
+    def __repr__(self):
+        return f"<User first_name:{self.first_name}, last_name: {self.last_name}, email: {self.email}>"
+    
+    @classmethod
+    def register(cls, username, email, password, img_url):
+        """Register user to the application given form data.
+
+        Hashes password and adds it to the database.
+        """
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        new_user = User(username=username, email=email, password=hashed_password, img_url=img_url)
+
+        db.session.add(new_user)
+
+        return new_user
 
 class Match(db.Model):
-    """ Mapping for user matching with other users.
+    """Mapping for user matching with other users.
+
     When users match with one another, they will be added to this table.
     """
 
@@ -45,21 +62,28 @@ class Match(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    user_a = db.Column(db.Integer, 
-                       db.ForeignKey("users.id"), 
+    user_a = db.Column(db.Integer,
+                       db.ForeignKey("users.id"),
                        primary_key=True)
 
-    user_b = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True) 
+    user_b = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+
 
 class Message(db.Model):
-    """ Messages between two users."""
+    """Messages between two users."""
 
     __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     message = db.Column(db.String, nullable=False)
-    user_from_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    user_to_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
+    user_from_id = db.Column(db.Integer,
+                             db.ForeignKey("users.id"),
+                             primary_key=True)
 
-    
+    user_to_id = db.Column(db.Integer,
+                           db.ForeignKey("users.id"),
+                           primary_key=True)
+
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now())
